fc07af858ddf3ab6df68685efc70b7b9c90887da
vitalis-virtus <vshayuk@gmail.com>
2022-09-06 13:38:38 +0300

    создал структуру User и два метода для нее

--- main.go
+++ main.go
@@ -5,9 +5,30 @@ import (
 	"net/http"
 ) // пакет для функцій по виводу інформації в консолі чи на сайті
 
+// створюємо структуру
+type User struct {
+	name                  string  // відразу треба вказувати тип даних
+	age                   uint16  //ціле невід'ємне число
+	money                 int16   //ціле число
+	avg_grades, happiness float64 //число з комою
+}
+
+// створюємо метод для структури
+func (u User) getAllInfo() string { // u -- об'єкт до якого ми будемо звертатись в методі
+	return fmt.Sprintf("Username is: %s. He is %d and he has money"+
+		" equal: %d.", u.name, u.age, u.money) // %s-для строки	%d-для числа
+}
+
+func (u *User) setNewName(newName string) { // треба передавати посилання на сам об'єкт, викор знак "*"
+	u.name = newName
+}
+
 func home_page(page http.ResponseWriter, r *http.Request) { // через параметр page ми зможемо звертатись до сторінки, показувати, виводити текст, HTML
 	// r параметр, який передається, ми зможемо відслідкувати дані при запиті/підключенні
-	fmt.Fprintf(page, "Go is super easy!") //ствроюємо форматовану строку, в яку можна динамічно підставляти значення
+	tom := User{"Tom", 35, -10, 2.2, 0.6}
+	tom.setNewName("Alex")
+	fmt.Fprint(page, tom.getAllInfo()) //ствроюємо форматовану строку, в яку можна динамічно підставляти значення
+	// fmt.Fprintf(page, tom.name)
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
@@ -21,5 +42,10 @@ func handleRequest() {
 }
 
 func main() {
+	// var bob User = ...
+
+	//наступні методи створення екземпляра структури однакові -- можна з ключамми, можна і без
+	// bob := User{name: "Bob", age: 25, money: -50, avg_grades: 4.2, happiness: 0.8}
+
 	handleRequest()
 }
