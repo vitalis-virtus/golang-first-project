7ddcd6f7bdf5b876e45f7c6f6c82991f31fb2a35
vitalis-virtus <vshayuk@gmail.com>
2022-09-06 12:23:30 +0300

    Добавил роут /contacts

--- main.go
+++ main.go
@@ -5,12 +5,21 @@ import (
 	"net/http"
 ) // пакет для функцій по виводу інформації в консолі чи на сайті
 
-func home_page(page http.ResponseWriter, r *http.Request) { // через параметр w ми зможемо звертатись до сторінки, показувати, виводити текст, HTML
+func home_page(page http.ResponseWriter, r *http.Request) { // через параметр page ми зможемо звертатись до сторінки, показувати, виводити текст, HTML
 	// r параметр, який передається, ми зможемо відслідкувати дані при запиті/підключенні
 	fmt.Fprintf(page, "Go is super easy!") //ствроюємо форматовану строку, в яку можна динамічно підставляти значення
 }
 
-func main() {
-	http.HandleFunc("/", home_page)   //роутинг -- при переході на "/" ми застосовуємо метод "home_page"
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Contacts page!")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page) //роутинг -- при переході на "/" ми застосовуємо метод "home_page"
+	http.HandleFunc("/contacts", contacts_page)
 	http.ListenAndServe(":8080", nil) // запуск локального сервера на порті 8080 / nil - аналог null / дргуий параметр -- настройки
+}
+
+func main() {
+	handleRequest()
 }
